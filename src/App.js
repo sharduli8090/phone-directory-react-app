1f3dab4eeb848b94bc652ababf850d6339b36099
Sharduli Pandey <shardulipandey8090@gmail.com>
2022-10-06 22:32:23 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import {type} from '@testing-library/user-event/dist/type';
 import React, {Component} from 'react';
+import Header from './Header.js';
 
-class App extends Component{
+class App extends Component {
 
-  render(){
-    return (
-      <div>
-        Phone Directory
-      </div>
-    );
-  }
+    render() {
+        return(
+          
+
+              <div>
+                <div className="header">
+                  Phone Directory
+                </div>
+                <Header/>
+                <button>Add</button>
+                <div>
+                  <span>  Name</span>
+                  <br/>
+                  <span>Phone</span>
+                </div>
+              </div>
+
+        );
+    }
 }
 
 
