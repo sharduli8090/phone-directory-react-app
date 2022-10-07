4b993b4aaf25599405139ab10eda9ecceabfe03e
Sharduli Pandey <shardulipandey8090@gmail.com>
2022-10-07 12:29:43 +0530

    Added subscribers dynamically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import {type} from '@testing-library/user-event/dist/type';
 import React, {Component} from 'react';
 import Header from './Header.js';
+import './App.css';
 
 
 class App extends Component {
 
     render() {
-        return(
-          
 
-              <div>
+        let subscribers = [
+            {
+                id: 1,
+                name: "Shardu",
+                phone: "8888888888"
+            }, {
+                id: 2,
+                name: "Chinu",
+                phone: "99999999999"
+            }
+        ]
+
+        return (
+            <div>
                 <Header/>
-                <button>Add</button>
-                <div>
-                  <span>  Name</span>
-                  <br/>
-                  <span>Phone</span>
+                <button id="AddBtn">Add</button>
+                <div className="btnFlex">
+                    <span className='headText'>
+                        Name</span>
+                    <span className='headText'>Phone</span>
+
                 </div>
-              </div>
+                {
+                subscribers.map(sub => {
+                    return <div key={
+                            sub.id
+                        }
+                        className="btnFlex">
+                        <span className='bodyText'>{sub.name}</span>
+                        <span className='bodyText'>{sub.phone}</span>
+                    </div>
+            })
+            } </div>
 
         );
     }
